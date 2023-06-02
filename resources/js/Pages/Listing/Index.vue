786ce7a82f3e3c77e0463acea5eb21ac3aa8617d
ChrisToxz <chris@toxz.nl>
2023-06-02 23:13:44 +0200

    Till 72

--- resources/js/Pages/Listing/Index.vue
+++ resources/js/Pages/Listing/Index.vue
@@ -1,9 +1,6 @@
 <script setup>
-import {Link} from '@inertiajs/vue3'
-import ListingAddress from '@/Components/ListingAddress.vue'
-import ListingSpace from '@/Components/UI/ListingSpace.vue'
-import Box from '@/Components/UI/Box.vue'
-import Price from '@/Components/Price.vue'
+
+import Listing from '@/Pages/Listing/Index/Components/Listing.vue'
 
 
 defineProps({
@@ -13,21 +10,7 @@ defineProps({
 </script>
 <template>
   <div class="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-4">
-    <Box v-for="listing in listings" :key="listing.id">
-      <div>
-        <Link :href="route('listing.show', listing.id)">
-          <Price :price="listing.price" class="text-2xl font-bold" />
-          <ListingSpace :listing="listing" class="text-lg" />
-          <ListingAddress :listing="listing" class="text-gray-400" />
-        </Link>
-      </div>
-      <div>
-        <Link :href="route('listing.edit', listing.id)">Edit</Link>
-      </div>
-      <div>
-        <Link :href="route('listing.destroy', listing.id)" method="delete" as="button">Delete</Link>
-      </div>
-    </Box>
+    <Listing v-for="listing in listings" :key="listing.id" :listing="listing" />
   </div>
 </template>
 
--- resources/js/Pages/Listing/Show.vue
+++ resources/js/Pages/Listing/Show.vue
@@ -3,8 +3,13 @@ import ListingAddress from '@/Components/ListingAddress.vue'
 import ListingSpace from '@/Components/UI/ListingSpace.vue'
 import Price from '@/Components/Price.vue'
 import Box from '@/Components/UI/Box.vue'
+import {ref} from 'vue'
 
-defineProps({
+
+const interestRate = ref(2.5)
+const duration = ref(25)
+
+const props = defineProps({
   listing: Object,
 },
 )
@@ -24,20 +29,22 @@ defineProps({
       <Box>
         <template #header>Monthly Payment</template>
         <div class="test">
-          <label class="label"> Interest rate (2.5%)</label>
+          <label class="label"> Interest rate ({{ interestRate }})</label>
           <input
-            type="range" min="0.1" max="30" step=".1"
+            v-model.number="interestRate"
+            type="range" min="0.1" max="30" step="0.1"
             class="w-full h-4 bg-gray-200 rounded-lg appearance-none cursor-pointer dark:bg-gray-700"
           />
-          <label class="label"> Duration (25 years)</label>
+          <label class="label"> Duration ({{ duration }} years)</label>
           <input
+            v-model.number="duration"
             type="range" min="3" max="35" step="1"
             class="w-full h-4 bg-gray-200 rounded-lg appearance-none cursor-pointer dark:bg-gray-700"
           />
 
           <div class="text-gray-600 dark:text-gray-300 mt-2" />
           <div class="text-gray-400">Your monthly payment</div>
-          <Price :price="500" class="text-3xl" />
+          <Price :price="monthlyPayment" class="text-3xl" />
         </div>
       </Box>
     </div>
