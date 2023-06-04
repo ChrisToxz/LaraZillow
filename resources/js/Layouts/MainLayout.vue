c1f248ae5f38e3d9b9ba14de6124e9c565b208c7
ChrisToxz <chris@toxz.nl>
2023-06-04 12:46:15 +0200

    Flash error

--- resources/js/Layouts/MainLayout.vue
+++ resources/js/Layouts/MainLayout.vue
@@ -7,6 +7,10 @@ const flashSuccess = computed(
   () => usePage().props.flash.success,
 )
 
+const flashError = computed(
+  () => usePage().props.flash.error,
+)
+
 const user = computed(
   () => usePage().props.user,
 )
@@ -41,6 +45,9 @@ const user = computed(
   <main class="container mx-auto p-4 w-full">
     <div v-if="flashSuccess" class="mb-4 border rounded-md shadow-sm border-green-200 dark:border-green-800 bg-green-50 dark:bg-green-900 p-2">
       {{ flashSuccess }}
+    </div>
+    <div v-if="flashError" class="mb-4 border rounded-md shadow-sm border-red-200 dark:border-red-800 bg-red-50 dark:bg-red-900 p-2">
+      {{ flashError }}
     </div>
     <slot>Default</slot>
   </main>
