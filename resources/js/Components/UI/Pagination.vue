bb0449fce3b889fe03d6da894051f880cc54298d
ChrisToxz <chris@toxz.nl>
2023-06-05 21:38:42 +0200

    Conditional Pagination if links.length has more than 3 values. (Prev, Next & Page 1)

--- resources/js/Components/UI/Pagination.vue
+++ resources/js/Components/UI/Pagination.vue
@@ -2,18 +2,21 @@
 import {Link} from '@inertiajs/vue3'
 
 defineProps({links: Array})
+
 </script>
 
 <template>
   <div class="flex gap-1">
-    <template v-for="(link, index) in links">
-      <Link
-        v-if="link.url"
-        :key="index" class="py-2 px-4 rounded-md hover:bg-indigo-400 dark:hover:bg-indigo-600"
-        :href="link.url"
-        :class="{'bg-indigo-500 dark:bg-indigo-800 text-gray-300': link.active}"
-        v-html="link.label"
-      />
+    <template v-if="links.length > 3">
+      <template v-for="(link, index) in links">
+        <Link
+          v-if="link.url"
+          :key="index" class="py-2 px-4 rounded-md hover:bg-indigo-400 dark:hover:bg-indigo-600"
+          :href="link.url"
+          :class="{'bg-indigo-500 dark:bg-indigo-800 text-gray-300': link.active}"
+          v-html="link.label"
+        />
+      </template>
     </template>
   </div>
 </template>
