ee29813bdc9bcae3cc4a852de441703cfb1fcb4f
ChrisToxz <chris@toxz.nl>
2023-06-05 21:29:38 +0200

    Fix pagination null error and add hover styling

--- resources/js/Components/UI/Pagination.vue
+++ resources/js/Components/UI/Pagination.vue
@@ -6,13 +6,15 @@ defineProps({links: Array})
 
 <template>
   <div class="flex gap-1">
-    <Link
-      v-for="(link, index) in links"
-      :key="index" class="py-2 px-4 rounded-md"
-      :href="link.url"
-      :class="{'bg-indigo-500 dark:bg-indigo-800 text-gray-300': link.active}"
-      v-html="link.label"
-    />
+    <template v-for="(link, index) in links">
+      <Link
+        v-if="link.url"
+        :key="index" class="py-2 px-4 rounded-md hover:bg-indigo-400 dark:hover:bg-indigo-600"
+        :href="link.url"
+        :class="{'bg-indigo-500 dark:bg-indigo-800 text-gray-300': link.active}"
+        v-html="link.label"
+      />
+    </template>
   </div>
 </template>
 
