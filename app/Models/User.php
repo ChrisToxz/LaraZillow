bf45e2b36df18989043cee1bfc2080801b822668
ChrisToxz <chris@toxz.nl>
2023-06-07 19:15:34 +0200

    Offers model & migration

--- app/Models/User.php
+++ app/Models/User.php
@@ -58,4 +58,9 @@ class User extends Authenticatable
     {
         return $this->hasMany(Listing::class);
     }
+
+    public function offers(): HasMany
+    {
+        return $this->hasMany(Offer::class);
+    }
 }
